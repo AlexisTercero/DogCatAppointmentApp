248b05fe6acc2884325e1f98db0880de696810b3
Alexis Tercero <alexisdealmeyda@gmail.com>
2021-06-11 13:58:15 -0300

    Styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="pushfoot"></div>
       <hr></hr>
       <div className="footer">
-        <p>Author: Alexis De Almeyda</p>
+        <p>ALEXIS DE ALMEYDA © 2021</p>
         <p>
           <a id="mail" href="mailto:alexisdealmeyda@gmail.com">
             alexisdealmeyda@gmail.com
